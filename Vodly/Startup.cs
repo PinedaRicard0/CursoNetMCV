33b66f6ca58c04cfe7859398ea70f66a2446c9f3
Ricardo Pineda <pinedaricardo07@gmail.com>
2019-01-11 02:46:49 -0500

    Subiendo proyecto base

---
+++ Vodly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vodly.Startup))]
+namespace Vodly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
